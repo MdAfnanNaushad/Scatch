26e2a84f508245a228bf39ccb4151d10f2d534f5
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-04-06 01:09:06 +0530

    Changes proceed

--- config/multer-config.js
+++ config/multer-config.js
@@ -1,5 +1,7 @@
 const multer = require("multer");
 const path = require("path");
+const express = require("express");
+const app = express();
 // Setting up multer storage to save files to the 'public/profilepic/uploads' folder
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -12,5 +14,9 @@ const storage = multer.diskStorage({
 
 // Initialize multer with the storage settings
 const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.send('File uploaded successfully');
+});
 
 module.exports = upload;  // Export multer configuration to be used in other files
