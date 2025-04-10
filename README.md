5b629a07b7e0415478b23e489b018c74dc8d327f
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-04-10 22:00:08 +0530

    Adding proper error handling routes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Scatch
+to run the file hit npm start || npx nodemon app.js
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose'); //requiring mongoose to connect thourgh database
 const config = require("config"); //Requiring config which typically used configuration files to interact with 
 const dbgr = require("debug")("development:mongoose");//This line imports the debug module, which is used for logging debug information. Here, it's namespaced as development:mongoose.
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)//this is the connection String generation
+const connectionString = process.env.MONGODB_URI || config.get("MONGODB_URI");
+mongoose.connect(connectionString)//this is the connection String generation
     .then(function () {
         dbgr("connected"); //This block attempts to connect to the MongoDB database using the connection string obtained from the configuration settings.
         console.log("connected to server");
